a123e842906cdf962dff76e50e6552df152734a9
Jason Hinton <jason_hinton@att.net>
2020-07-17 23:55:46 -0400

    put done

--- data/routers/actions-router.js
+++ data/routers/actions-router.js
@@ -46,6 +46,21 @@ router.post("/", (req, res) => {
 		});
 });
 
-
+router.put("/:id", (req, res) => {
+	const changes = req.body;
+	Actions.update(req.params.id, changes)
+		.then((action) => {
+			if (action) {
+				res.status(200).json(action);
+			} else {
+				res.status(404).json({ message: "Action not found." });
+			}
+		})
+		.catch((err) => {
+			res
+				.status(500)
+				.json({ message: "There was an error updating the action.", err });
+		});
+});
 
 module.exports = router;
--- data/routers/projects-router.js
+++ data/routers/projects-router.js
@@ -31,25 +31,34 @@ router.get("/:id", (req, res) => {
 		});
 });
 
-
 router.post("/", (req, res) => {
 	Projects.insert(req.body)
 		.then((project) => {
 			res.status(201).json(project);
 		})
 		.catch((err) => {
-			res
-				.status(500)
-				.json({
-					message: "There was a problem adding the project to the database.",
-					err,
-				});
+			res.status(500).json({
+				message: "There was a problem adding the project to the database.",
+				err,
+			});
 		});
 });
 
-
-router.put('/:id', (req, res) => {
-  
-})
+router.put("/:id", (req, res) => {
+	const changes = req.body;
+	Projects.update(req.params.id, changes)
+		.then((project) => {
+			if (project) {
+				res.status(200).json(project);
+			} else {
+				res.status(404).json({ message: "Project not found." });
+			}
+		})
+		.catch((err) => {
+			res
+				.status(500)
+				.json({ message: "There was an error updating the project.", err });
+		});
+});
 
 module.exports = router;

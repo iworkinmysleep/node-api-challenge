77839f29294accfba583b5a2371f4acfa490e022
Jason Hinton <jason_hinton@att.net>
2020-07-18 00:30:59 -0400

    mvp

--- data/routers/projects-router.js
+++ data/routers/projects-router.js
@@ -61,7 +61,6 @@ router.put("/:id", (req, res) => {
 		});
 });
 
-
 router.delete("/:id", (req, res) => {
 	Projects.remove(req.params.id)
 		.then((count) => {
@@ -76,6 +75,23 @@ router.delete("/:id", (req, res) => {
 				.status(500)
 				.json({ message: "There was an error removing the project.", err });
 		});
+});
+
+router.get("/:id/actions", async (req, res) => {
+	try {
+		const actions = await Projects.getProjectActions(req.params.id);
+		if (actions.length > 0) {
+			res.status(200).json(actions);
+		} else {
+			res
+				.status(404)
+				.json({ message: "There are no actions for the project." });
+		}
+	} catch (err) {
+		res
+			.status(500)
+			.json({ message: "There was an error displaying actions.", err });
+	}
 });
 
 module.exports = router;
